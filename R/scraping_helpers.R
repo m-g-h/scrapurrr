aae1052168cfc33536bd6dff8d37f0ca31d84bf4
m-g-h <haasmartin@live.de>
2021-09-24 10:16:58 +0200

    Added node_which() and html_find()

--- R/scraping_helpers.R
+++ R/scraping_helpers.R
@@ -37,9 +37,103 @@ return_named_list <- function() {
 
   # Replace NULL values by NA
   list[sapply(list, is.null) %>%
-    which()] = NA
+         which()] = NA
 
   list
 
 
+}
+
+#' Find the positions of nodes in a nodelist that matche a regex pattern
+#'
+#' @param nodelist \code{xml_nodeset}, e.g. returned from \code{rvest}s
+#' \code{link{html_elements}}
+#' @param regex \code{string scalar} giving the regex to search for. See the
+#' stringr cheatsheet on \url{https://www.rstudio.com/resources/cheatsheets/}
+#' @param inc \code{numeric scalar} Increment added to the returned index. See
+#' examples for a usecase.
+#'
+#' @return Returns a \code{numeric scalar or vector}
+#' @export
+#'
+#' @importFrom stringr str_which
+#' @importFrom rvest html_text
+#'
+#' @examples
+#' library(rvest)
+#' library(webscraping)
+#'
+#' # Lets suppose we want to know the owner of "Alfreds Futterkiste":
+#' html = "<table>
+#'   <tr>
+#'     <th>Company</th>
+#'     <th>Contact</th>
+#'     <th>Country</th>
+#'   </tr>
+#'   <tr>
+#'     <td>Alfreds Futterkiste</td>
+#'     <td>Maria Anders</td>
+#'     <td>Germany</td>
+#'   </tr>
+#'   <tr>
+#'     <td>Centro comercial Moctezuma</td>
+#'     <td>Francisco Chang</td>
+#'     <td>Mexico</td>
+#'   </tr>
+#' </table>" %>%
+#'   read_html()
+#'
+#' # Searching for `td` elements returns a list:
+#' html_elements(x = html, "td")
+#' # Of course we could match by position, but it may not be fixed if we have
+#' # many tables. Let's use `node_which()`. since the "owner" is always two rows
+#' # behind the "company" we increment by 2:
+#' html_elements(x = html, "td") %>%
+#'   node_which("Alfreds Futterkiste", inc = 2)
+
+node_which <- function(nodelist, regex, inc = 0) {
+  str_which(html_text(nodelist), regex) + inc
+}
+
+#' Find and return the node(s) in a nodelist that matche a regex pattern
+#'
+#' @inheritParams node_which
+#'
+#' @return Returns a \code{xml_nodeset}
+#' @export
+#'
+#' @examples
+#' library(rvest)
+#' library(webscraping)
+#'
+#' # Lets suppose we want to know the owner of "Alfreds Futterkiste":
+#' html = "<table>
+#'   <tr>
+#'     <th>Company</th>
+#'     <th>Contact</th>
+#'     <th>Country</th>
+#'   </tr>
+#'   <tr>
+#'     <td>Alfreds Futterkiste</td>
+#'     <td>Maria Anders</td>
+#'     <td>Germany</td>
+#'   </tr>
+#'   <tr>
+#'     <td>Centro comercial Moctezuma</td>
+#'     <td>Francisco Chang</td>
+#'     <td>Mexico</td>
+#'   </tr>
+#' </table>" %>%
+#'   read_html()
+#'
+#' # Searching for `td` elements returns a list:
+#' html_elements(x = html, "td")
+#' # Of course we could match by position, but it may not be fixed if we have
+#' # many tables. Let's use `node_which()`. since the "owner" is always two rows
+#' # behind the "company" we increment by 1:
+#' html_elements(x = html, "td") %>%
+#'   html_find("Alfreds Futterkiste", inc = 1)
+
+html_find <- function(nodelist, regex, inc = 0) {
+  nodelist[node_which(nodelist, regex, inc)]
 }
--- tests/testthat/test-scraping_helpers.R
+++ tests/testthat/test-scraping_helpers.R
@@ -20,3 +20,80 @@ test_that("return_named_list() works", {
                       id = x))
 
 })
+
+test_that("node_which() works",{
+
+  # Lets suppose we want to know the owner of "Alfreds Futterkiste":
+  html = "<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>" %>%
+    xml2::read_html()
+
+  # Searching for `td` elements returns a list:
+  rvest::html_elements(x = html, "td")
+  # Of course we could match by position, but it may not be fixed if we have
+  # many tables. Let's use `node_which()`. since the "owner" is always two rows
+  # behind the "company" we increment by 2:
+  rvest::html_elements(x = html, "td") %>%
+    node_which("Alfreds Futterkiste", inc = 2)
+
+  # Test that position equals 3
+  expect_equal(rvest::html_elements(x = html, "td") %>%
+                 node_which("Alfreds Futterkiste", inc = 2),
+               3)
+}
+)
+
+test_that("html_find() works",{
+  # Lets suppose we want to know the owner of "Alfreds Futterkiste":
+  html = "<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>" %>%
+    xml2::read_html()
+
+  # Searching for `td` elements returns a list:
+  rvest::html_elements(x = html, "td")
+  # Of course we could match by position, but it may not be fixed if we have
+  # many tables. Let's use `node_which()`. since the "owner" is always two rows
+  # behind the "company" we increment by 1:
+  rvest::html_elements(x = html, "td") %>%
+    html_find("Alfreds Futterkiste", inc = 1)
+
+  result = rvest::html_elements(x = html, "td") %>%
+    .[2]
+
+  # Test that position equals 3
+  expect_equal(rvest::html_elements(x = html, "td") %>%
+                 html_find("Alfreds Futterkiste", inc = 1),
+               result
+  )
+}
+)
